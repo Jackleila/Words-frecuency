543bc0d8da4bc859d6925c43ca9a10ea8e732aea
Sara <Jackleila@users.noreply.github.com>
2019-02-08 22:53:07 +0100

    Create nltkTest.py

---
+++ nltkTest.py
@@ -0,0 +1,31 @@
+import nltk
+import matplotlib.pyplot as ptl
+from nltk.corpus import stopwords 
+
+#Language selection
+language = input("Language: ")
+
+#Opening and reading file
+with open(input("Enter Filename: "), 'r') as myfile:
+    data=myfile.read()
+
+#Tokenizing
+tokens = [t for t in data.split()]
+
+#Removing stop words
+clean_tokens = tokens[:]
+sr = stopwords.words(language)
+ 
+for token in tokens:
+    if token in sr:
+        clean_tokens.remove(token)
+
+#Getting the frecuency
+freq = nltk.FreqDist(clean_tokens)
+ 
+for key,val in freq.items():
+    print (str(key) + ':' + str(val))
+
+#Plotting
+freq.plot(20, cumulative=False)
+ptl.show()
